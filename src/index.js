960228dc50cb5a7dbff37fcb9cc5d7a579b13425
Syllinger <syllinger@gmail.com>
2017-03-27 00:01:22 -0400

    Create video list container and video list items.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/serach_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAHnHZSMAopkrkIAN0yuSWeNxb3ldsprQc';
 
@@ -18,6 +19,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
