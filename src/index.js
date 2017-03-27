e91c50b7f2edbae2f9525b72e30cfbe5bd679760
Syllinger <syllinger@gmail.com>
2017-03-26 23:27:30 -0400

    Create search bar component.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/serach_bar';
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyAHnHZSMAopkrkIAN0yuSWeNxb3ldsprQc';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, data => {console.log(data)});
 
 const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
